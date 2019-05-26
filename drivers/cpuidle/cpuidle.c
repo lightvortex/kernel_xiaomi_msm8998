27c23da7a9a443b72494816cf8c6b1183864b946
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-26 10:18:46 -0700

    cpuidle: Optimize pm_qos notifier callback
    The pm_qos notifier currently suffers from a number of pitfalls: it
    sends IPIs to CPUs that may not be idle, waits for the IPIs to finish
    propagating while preemption is disabled (resulting in a long busy wait
    for the pm_qos_update_target caller), needlessly calls a no-op
    function when the IPIs are processed, and sends IPIs even when the CPU
    latency requirement is relaxed.
    
    Optimize the pm_qos notifier by using arch_send_call_function_ipi_mask
    instead of smp_call_function_many to eliminate the need to call a no-op
    function (with the added benefit of it not blocking) and only sending
    IPIs to CPUs that are idle. Additionally, only send IPIs when the CPU
    latency requirement is tightened, since it's not necessary to kick CPUs
    out of idle when the latency requirement is relaxed.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Angga Satya Putra <anggasp@hotmail.com>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -34,7 +34,9 @@ LIST_HEAD(cpuidle_detected_devices);
 static int enabled_devices;
 static int off __read_mostly;
 static int initialized __read_mostly;
-static atomic_t idle_cpus = ATOMIC_INIT(0);
+
+static void cpuidle_set_idle_cpu(unsigned int cpu);
+static void cpuidle_clear_idle_cpu(unsigned int cpu);
 
 int cpuidle_disabled(void)
 {
@@ -200,9 +202,9 @@ int cpuidle_enter_state(struct cpuidle_device *dev, struct cpuidle_driver *drv,
 	time_start = ktime_get();
 
 	stop_critical_timings();
-	atomic_or(BIT(dev->cpu), &idle_cpus);
+	cpuidle_set_idle_cpu(dev->cpu);
 	entered_state = target_state->enter(dev, drv, index);
-	atomic_andnot(BIT(dev->cpu), &idle_cpus);
+	cpuidle_clear_idle_cpu(dev->cpu);
 	start_critical_timings();
 
 	time_end = ktime_get();
@@ -616,6 +618,22 @@ int cpuidle_register(struct cpuidle_driver *drv,
 EXPORT_SYMBOL_GPL(cpuidle_register);
 
 #ifdef CONFIG_SMP
+static atomic_t idle_cpu_mask = ATOMIC_INIT(0);
+
+#if NR_CPUS > 32
+#error idle_cpu_mask not big enough for NR_CPUS
+#endif
+
+static void cpuidle_set_idle_cpu(unsigned int cpu)
+{
+	atomic_or(BIT(cpu), &idle_cpu_mask);
+}
+
+static void cpuidle_clear_idle_cpu(unsigned int cpu)
+{
+	atomic_andnot(BIT(cpu), &idle_cpu_mask);
+}
+
 /*
  * This function gets called when a part of the kernel has a new latency
  * requirement.  This means we need to get only those processors out of their
@@ -625,19 +643,28 @@ EXPORT_SYMBOL_GPL(cpuidle_register);
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	static unsigned long prev_latency = ULONG_MAX;
-	unsigned long cpus;
-	struct cpumask *idle_mask = to_cpumask(&cpus);
+	static unsigned long prev_latency[NR_CPUS] = {
+		[0 ... NR_CPUS - 1] = PM_QOS_CPU_DMA_LAT_DEFAULT_VALUE
+	};
+	struct cpumask update_mask = CPU_MASK_NONE;
+	unsigned int cpu;
 
-	if (l < prev_latency) {
-		preempt_disable();
-		cpus = atomic_read(&idle_cpus);
-		cpumask_andnot(idle_mask, idle_mask, cpu_isolated_mask);
-		arch_send_call_function_ipi_mask(idle_mask);
-		preempt_enable();
+	/* Only send an IPI when the CPU latency requirement is tightened */
+	for_each_cpu(cpu, v) {
+		if (l < prev_latency[cpu])
+			cpumask_set_cpu(cpu, &update_mask);
+		prev_latency[cpu] = l;
 	}
 
-	prev_latency = l;
+	if (!cpumask_empty(&update_mask)) {
+		unsigned long idle_cpus = atomic_read(&idle_cpu_mask);
+
+		cpumask_and(&update_mask, &update_mask, to_cpumask(&idle_cpus));
+		cpumask_andnot(&update_mask, &update_mask, cpu_isolated_mask);
+
+		/* Notifier is called with preemption disabled */
+		arch_send_call_function_ipi_mask(&update_mask);
+	}
 
 	return NOTIFY_OK;
 }
@@ -653,6 +680,14 @@ static inline void latency_notifier_init(struct notifier_block *n)
 
 #else /* CONFIG_SMP */
 
+static void cpuidle_set_idle_cpu(unsigned int cpu)
+{
+}
+
+static void cpuidle_clear_idle_cpu(unsigned int cpu)
+{
+}
+
 #define latency_notifier_init(x) do { } while (0)
 
 #endif /* CONFIG_SMP */
@@ -664,7 +699,6 @@ static int __init cpuidle_init(void)
 {
 	int ret;
 
-	BUILD_BUG_ON(NR_CPUS > sizeof(idle_cpus.counter) * 8);
 	if (cpuidle_disabled())
 		return -ENODEV;
 
